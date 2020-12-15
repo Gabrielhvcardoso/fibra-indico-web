2ee73a06cc71017d2fefb35febe2b8641969eb4a
Gabrielhvcardoso <gabrielhvcardoso@gmail.com>
2020-12-15 16:23:46 -0300

    hierarchy item style

--- src/pages/Products/components/Controller/components/HierarchyItem/styles.ts
+++ src/pages/Products/components/Controller/components/HierarchyItem/styles.ts
@@ -5,19 +5,21 @@ export const Container = styled.div`
   border-radius: 10px;
   display: flex;
   flex-direction: column;
-  margin: 5px 0px;
-  padding: 15px;
+  margin: 5px 0px 10px;
+  padding: 15px 15px 5px 15px;
 `;
 
 export const Text = styled.p`
   margin: 0px;
+  text-align: center;
 `;
 
 export const Button = styled.button`
   align-self: stretch;
-  /* background-color: transparent; */
+  background-color: transparent;
   border: none;
   color: #2b7ed7;
-  margin-top: 10px;
+  cursor: pointer;
+  margin-top: 5px;
   padding: 7px 0px;
 `;
