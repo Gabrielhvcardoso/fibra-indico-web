ac96b08ff9578a1e37919a51709f0c900bf151b2
Gabriel Cardoso <gabrielhvcardoso@gmail.com>
2021-01-01 16:01:46 -0300

    new users

--- src/pages/Panel/Registers/index.tsx
+++ src/pages/Panel/Registers/index.tsx
@@ -1,8 +1,58 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { useContext, useEffect, useState } from 'react';
+import { useFetch } from '../../../hooks';
+import { Container, ListItem, Column } from './styles';
+import update from 'immutability-helper';
+
+import AuthContext from '../../../context/auth';
+import { User } from '../../../models/User';
+import { Button } from '../Recommendations/styles';
+import Alert from '../../../components/Alert';
 
 const Registers: React.FC = () => {
-  return <Container />;
+  const { secret } = useContext(AuthContext);
+  const [users, setUsers] = useState<Array<User>>([]);
+  const [error, setError] = useState<null | string>(null);
+
+  useEffect(() => {
+    useFetch.get(`/m/u/n/${secret}`, (response) => {
+      setUsers(response.users);
+    });
+  }, []);
+
+  const ativateUser = (token: string) => {
+    useFetch.get(`/m/u/a/${token}/${secret}`, (response) => {
+      if (response.code === 'error') {
+        return setError('Não foi possível ativar o usuário, por favor, entre em contato com o desenvolvedor caso o problema continue');
+      }
+
+      const index = users.findIndex(({ token: userToken }) => userToken === token);
+      setUsers(update(users, {
+        $splice: [[index, 1]]
+      }));
+    });
+  };
+
+  return (
+    <Container>
+      <Alert visible={Boolean(error)} onDismiss={() => setError(null)} timeout={4000}>
+        { error }
+      </Alert>
+      {
+        users.map((item) => (
+          <ListItem onClick={() => ativateUser(item.token)} key={item.token}>
+            <Column>
+              <span>{ item.name }</span>
+              <small>{ item.city } - { item.state }</small>
+              <small>{ item.email }</small>
+              <small>{ item.phone }</small>
+            </Column>
+
+            <Button>Ativar conta</Button>
+          </ListItem>
+        ))
+      }
+    </Container>
+  );
 };
 
 export default Registers;
--- src/pages/Panel/Registers/styles.tsx
+++ src/pages/Panel/Registers/styles.tsx
@@ -4,3 +4,21 @@ export const Container = styled.div`
     flex: 1;
     background-color: #eee;
 `;
+
+export const ListItem = styled.div`
+  align-items: center;
+  color: black;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 10px;
+  padding: 15px 25px;
+
+  &:hover {
+    background-color: #00000011;
+  }
+`;
+
+export const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
